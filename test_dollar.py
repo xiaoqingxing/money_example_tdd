623c10180659760a04a77dea82c618df1fce952e
heroestk <qingxing.xiao@gmail.com>
2017-06-09 15:50:33 +0800

    chapter 2 Remove side effect of Dollar class

--- test_dollar.py
+++ test_dollar.py
@@ -7,13 +7,14 @@ class Dollar:
         self.amount = amount
 
     def times(self, multiplier):
-        self.amount *= multiplier
+        return Dollar(self.amount * multiplier)
 
 
 class TestMultiplication(unittest.TestCase):
 
     def test_multiplication(self):
         five = Dollar(5)
-        five.times(2)
-        self.assertEqual(10, five.amount)
-    pass
+        product = five.times(2)
+        self.assertEqual(10, product.amount)
+        product = five.times(3)
+        self.assertEqual(15, product.amount)
